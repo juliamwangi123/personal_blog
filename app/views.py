b5247aca9d10035a923635fdc601d55849493c64
juliamwangi123 <julia.mwangi@student.moringaschool.com>
2022-05-13 18:54:22 +0300

    ft:add about section

--- app/views.py
+++ app/views.py
@@ -1,7 +1,15 @@
 from app import app
 from flask import render_template
 
+
+# home page route
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('index.html')
+
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
