412ba1adc489db72676da96a50129c04f31a17f2
juliamwangi123 <julia.mwangi@student.moringaschool.com>
2022-05-13 23:11:33 +0300

    ft:initialize db and migrate engine

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
-
+from .config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import  Migrate
 # make an instance from flask class
 app=Flask(__name__)
 
+app.config.from_object(Config)
+#db object that represent database
+db=SQLAlchemy(app)
+#object representing migration  engine
+migrate=Migrate(app, db)
 
 from app import views
