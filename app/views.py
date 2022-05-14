0a22fc80133992923cfcaa88ef64845f4d9e678b
juliamwangi123 <julia.mwangi@student.moringaschool.com>
2022-05-14 09:37:20 +0300

    ft:add a route  to add  new blog

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import render_template
-
+from .forms import Blog
 
 # home page route
 @app.route('/')
@@ -9,7 +9,14 @@ def home():
     return render_template('index.html')
 
 
-
+#route to about section
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+#route to new blog
+@app.route('/new_blog' , methods=['GET', 'POST'])
+def new_blog():
+    form=Blog()
+    return render_template('newblog.html' ,form=form)
