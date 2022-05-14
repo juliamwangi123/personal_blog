b8a284069b9387046581abca40aceb59a86df89a
juliamwangi123 <julia.mwangi@student.moringaschool.com>
2022-05-14 09:18:06 +0300

    ft:add forms class to take in blog content

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,12 @@
+from turtle import title
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired
+from flask_wtf.file import FileField
 
+class  Blog(FlaskForm):
+    title=StringField('Title', validators=[DataRequired()])
+    sub_title=StringField('Sub title', validators=[StringField()])
+    image=FileField('Blog Image', validators=[DataRequired()])
+    content=StringField('Content', validators=[DataRequired()])
+    
