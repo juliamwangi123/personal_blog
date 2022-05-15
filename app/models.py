7cb413660ff8e36f9e893a9548ab3473f6d35570
juliamwangi123 <julia.mwangi@student.moringaschool.com>
2022-05-15 23:22:28 +0300

    add comment table in db

--- app/models.py
+++ app/models.py
@@ -12,7 +12,7 @@ class Blog(UserMixin,db.Model):
     content=db.Column(db.String(300))
     time_posted=db.Column(db.DateTime,index=True, default=datetime.now)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
+    comments = db.relationship('Comment', backref='article', lazy=True)
     
 
     def __repr__(self):
@@ -28,6 +28,7 @@ class User(UserMixin,db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     blogs = db.relationship('Blog', backref='author', lazy='dynamic')
+    comments=db.relationship('Comment' ,backref='writer', lazy='dynamic')
 
 
 
@@ -42,3 +43,12 @@ class User(UserMixin,db.Model):
         return '<User {}>'.format(self.username)
 
 
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(100), nullable=False) 
+    timestamp = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    author=db.Column(db.Integer, db.ForeignKey('user.id'))
+    blog_id = db.Column(db.Integer, db.ForeignKey('blog.id'))
+
+    def __repr__(self):
+        return f"Comment('{self.body}', '{self.blog_id}')"
